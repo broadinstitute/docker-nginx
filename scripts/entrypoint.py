d60bed11cac33a54d8e6fc48ef161c57a71ceed7
iromli <isman.firmansyah@gmail.com>
2019-07-15 06:21:03 +0700

    conform to API changes in pygluu.containerlib

--- scripts/entrypoint.py
+++ scripts/entrypoint.py
@@ -1,22 +1,13 @@
 from pygluu.containerlib import get_manager
 
-manager = get_manager()
 
-
-def render_ssl_cert():
-    ssl_cert = manager.secret.get("ssl_cert")
-    if ssl_cert:
-        with open("/etc/certs/gluu_https.crt", "w") as fd:
-            fd.write(ssl_cert)
-
-
-def render_ssl_key():
-    ssl_key = manager.secret.get("ssl_key")
-    if ssl_key:
-        with open("/etc/certs/gluu_https.key", "w") as fd:
-            fd.write(ssl_key)
+def main(manager):
+    # pull SSL cert to a file
+    manager.secret.to_file("ssl_cert", "/etc/certs/gluu_https.crt")
+    # pull SSL key to a file
+    manager.secret.to_file("ssl_key", "/etc/certs/gluu_https.key")
 
 
 if __name__ == "__main__":
-    render_ssl_cert()
-    render_ssl_key()
+    manager = get_manager()
+    main(manager)
