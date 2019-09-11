f995f192b444222e76bc8e35178e8eac3eb77cbf
iromli <isman.firmansyah@gmail.com>
2019-09-11 20:52:11 +0700

    don't pull self-signed cert and key if there are mounted files

--- scripts/entrypoint.py
+++ scripts/entrypoint.py
@@ -1,13 +1,19 @@
+import os
+
 from pygluu.containerlib import get_manager
 
 
-def main(manager):
-    # pull SSL cert to a file
-    manager.secret.to_file("ssl_cert", "/etc/certs/gluu_https.crt")
-    # pull SSL key to a file
-    manager.secret.to_file("ssl_key", "/etc/certs/gluu_https.key")
+def main():
+    manager = get_manager()
+
+    if not os.path.isfile("/etc/certs/gluu_https.crt"):
+        # pull SSL cert to a file
+        manager.secret.to_file("ssl_cert", "/etc/certs/gluu_https.crt")
+
+    if not os.path.isfile("/etc/certs/gluu_https.key"):
+        # pull SSL key to a file
+        manager.secret.to_file("ssl_key", "/etc/certs/gluu_https.key")
 
 
 if __name__ == "__main__":
-    manager = get_manager()
-    main(manager)
+    main()
